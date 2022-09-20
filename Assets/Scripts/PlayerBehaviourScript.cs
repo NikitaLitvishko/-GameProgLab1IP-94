aa1983d8b4d516b0e7e439614481caa2b2ac3cfa
NikitaLitvishko <55049232+NikitaLitvishko@users.noreply.github.com>
2022-09-20 18:03:08 +0300

    removed comments

--- Assets/Scripts/PlayerBehaviourScript.cs
+++ Assets/Scripts/PlayerBehaviourScript.cs
@@ -7,25 +7,18 @@ public class PlayerBehaviourScript : MonoBehaviour
     public float Speed = 10f;
     public bool isJumping;
     private Rigidbody2D rb;
-    // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
     void Update()
     {
         float moveX = Input.GetAxis("Horizontal");// -1 to 1
-        //rb.MovePosition(rb.position + Vector2.right * moveX * Speed * Time.deltaTime);
-
         rb.velocity = new Vector2(Speed * moveX, rb.velocity.y);
 
         if (Input.GetKeyDown(KeyCode.Space) && !isJumping)
-        {
-            //rb.AddForce(Vector2.up * 120 * Speed);
             rb.AddForce(new Vector2(rb.velocity.x, 1000));
-        }
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
